0e8cb9b07f717188e1a1666c7fd32a42cfb3e523
Mohamed Saqer <mohamed.saqer@outlook.com>
2016-08-21 04:18:07 +0200

    21-08.01

---
+++ Assets/Scripts/BasicControler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicControler : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Horizontal Input = "+Input.GetAxis("Horizontal"));
+	}
+}
---
+++ Assets/Scripts/BasicTragetMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTragetMover : MonoBehaviour {
+
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+	public bool doSpain = true;
+	public bool doMoation = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (doSpain) {
+			// Rotate around yield axsis
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+		}
+
+		if (doMoation) {
+
+			// move object
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagnitude);
+		}
+	}
+}
